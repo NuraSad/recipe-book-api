6df27ce67891ede931f6bcbfab34869275a3ae26
Nura <ainur.saduova10@gmail.com>
2023-09-18 17:10:05 -0400

    remove bodyParser

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./db");
 const app = express();
 const apiPort = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const recipeRouter = require("./routes/recipe-router");
