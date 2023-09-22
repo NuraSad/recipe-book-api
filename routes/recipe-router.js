c88c7193cd1736ba73f20b6bb1b55e42eeaf570c
Nura <ainur.saduova10@gmail.com>
2023-09-22 12:46:39 -0400

    change /recipe to /recipes

--- routes/recipe-router.js
+++ routes/recipe-router.js
@@ -4,10 +4,10 @@ const RecipeCtrl = require("../controllers/recipe-ctrl");
 
 const router = express.Router();
 
-router.post("/recipe", RecipeCtrl.createRecipe);
-router.put("/recipe/:id", RecipeCtrl.updateRecipe);
-router.delete("/recipe/:id", RecipeCtrl.deleteRecipe);
-router.get("/recipe/:id", RecipeCtrl.getRecipeById);
+router.post("/recipes", RecipeCtrl.createRecipe);
+router.put("/recipes/:id", RecipeCtrl.updateRecipe);
+router.delete("/recipes/:id", RecipeCtrl.deleteRecipe);
+router.get("/recipes/:id", RecipeCtrl.getRecipeById);
 router.get("/recipes", RecipeCtrl.getRecipes);
 
 module.exports = router;
