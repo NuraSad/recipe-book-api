f3ec9832c0c8bf433c3eb269e8c094dcdf5bd166
Nura <ainur.saduova10@gmail.com>
2024-01-12 15:45:03 -0500

    remove unnecessary respond info in signin route

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -52,9 +52,7 @@ exports.signin = (req, res) => {
     });
 
     res.status(200).send({
-      id: user._id,
       username: user.username,
-      email: user.email,
       accessToken: token,
     });
   });
